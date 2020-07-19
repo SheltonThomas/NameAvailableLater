31eee6fdd6b5833b1f8c9c4f0a9efd19575ae253
Ponder213 <67972215+Ponder213@users.noreply.github.com>
2020-07-19 12:14:06 -0500

    Added Shooting
    Shooting is added on left click with collision and a 1 second cooldown but no damage. Dashing still doesn't work everytime space is pressed

---
+++ Assets/Arrow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+
+    //public GameObject hitEffect; // can be used to add a on hit effect
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Instantiate(hitEffect, transform.position, Quaternion.identity);
+        //Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Shooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject arrowPrefab;
+    public float fireDelta = 1.0f;
+
+    public float arrowForce = 10f;
+    private float myTime;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        myTime += Time.deltaTime;
+        if (Input.GetButtonDown("Fire1") && myTime > fireDelta)
+        {
+            Shoot();
+            myTime = 0.0f;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject arrow = Instantiate(arrowPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * arrowForce, ForceMode2D.Impulse);
+
+    }
+}
